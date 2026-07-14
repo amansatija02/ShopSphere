df3c30373a0f0d69192d509394be5bf96f732538
amansatija02 <amansatija2006@gmail.com>
2026-07-14 20:34:35 +0530

    Add files via upload

---
+++ ShopSphere/include/core/AppConfig.hpp
@@ -0,0 +1,16 @@
+#pragma once 
+#include <string>
+using namespace std;
+
+namespace Config{
+const string PROJECT_NAME = "ShopSphere";
+const string VERSION = "1.0";
+const string RECORD_FOLDER = "records/";
+const string USER_RECORD = "records/users.txt";
+const string USER_DATA = "records/users.txt";
+const string CART_RECORD = "records/cart.txt";
+const string PRODUCT_RECORD = "records/products.txt";
+const string ORDER_RECORD = "records/orders.txt";
+const string ORDER_ITEM_RECORD = "records/order_items.txt";
+const string AUDIT_RECORD = "records/audit.txt";
+}
