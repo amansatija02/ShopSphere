6c0e517c8896b3a5adf8bb2190801fcbc4426f14
aman-reo <mishra.aman0605@gmail.com>
2026-07-18 13:54:44 +0530

    Add files via upload

---
+++ ShopSphere/include/input/InputGuard.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class InputGuard{
+
+public:
+
+    bool isEmpty(string value);
+
+    bool validEmail(string email);
+
+    bool validPassword(string password);
+
+};
